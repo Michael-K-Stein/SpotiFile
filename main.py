1b307712ebfa62c76d6440047a96b143760bf331
Michael Kuperfish Steinberg <36902556+Michael-K-Stein@users.noreply.github.com>
2023-01-05 14:53:09 +0200

    Fix startup to new name & tab openning

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from webgui import app
 import spotify_mass_download
 from spotify_mass_download import full_download, save_globals_save_file
 from threading import Thread
+import webbrowser
 
 def main():
-    print(f'Spotify Fuzzer')
-    print('\n\n\n')
-
+    print(f'=== SpotiFile ===')
     spotify_mass_download.g_keep_saving += 1
 
     save_globals_save_file_thread = Thread(target=save_globals_save_file)
     save_globals_save_file_thread.start()
+
+    webbrowser.open('http://127.0.0.1:8888/')
     app.run(host='127.0.0.1', port=8888, debug=False)
 
     spotify_mass_download.g_keep_saving -= 1
